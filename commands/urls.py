d97af482d52b4c8799a0947a621e74cda5d40a22
kailasam21 <161005156+kailasam21@users.noreply.github.com>
2024-04-30 11:10:16 +0530

    Add files via upload

---
+++ commands/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from showscreen import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name="Home"),
+    path('delete/<CName>',views.delete_city,name="DCity")
+]
